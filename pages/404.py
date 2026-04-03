f4a11747e82933cfa43046723428f777c79dd565
Cowsay <correctizer@gmail.com>
2026-04-03 18:01:26 +0800

    pages: index and 404

---
+++ pages/404.py
@@ -0,0 +1,4 @@
+from website.fs import write_page
+
+
+write_page("404.html", "undefined")
--- pages/index.py
+++ pages/index.py
@@ -8,7 +8,7 @@ from website.templates.base import Page
 content = f"""<div class="site-logo"><img src="{asset_url('logo.png')}" alt="Little Bot's Blog"></div>
 <h1>Little Bot's Blog</h1>
 <h2 style="margin-top: 0;">人是预测机器，偶尔许愿</h2>
-<p>你可以<a href="/search/">借助AI搜索内容</a>。以下是本站所有页面的链接。</p>
+<p>你可以<a href="/search/">借助AI搜索内容</a>。以下列出本站所有页面的链接以保证基本的连通性。</p>
 """
 for page in glob(f"{BUILD_DIR}/**/index.html", recursive=True):
     path = Path(page).parent.relative_to(BUILD_DIR)
