b82083f0d429a434e9253c0d569c5da31dedbe28
Cowsay <correctizer@gmail.com>
2026-04-04 11:12:21 +0800

    style

--- website/components/head.py
+++ website/components/head.py
@@ -10,5 +10,8 @@ class Head:
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{self.title}</title>
+    <link rel="preconnect" href="https://fastly.jsdelivr.net" crossorigin>
+    <link rel="preload" href="https://fastly.jsdelivr.net/npm/cn-fontsource-fz-kai-z-03-regular@1.0.1/font.css" as="style" crossorigin>
+    <link rel="stylesheet" href="https://fastly.jsdelivr.net/npm/cn-fontsource-fz-kai-z-03-regular@1.0.1/font.css" crossorigin>
     <link rel="stylesheet" href="{asset_url('main.css')}">
 </head>"""
